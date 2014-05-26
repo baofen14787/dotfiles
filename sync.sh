d7c296e87da8013cf628c46403d172f1a833af76
Isken Huang <kensheep@gmail.com>
2014-05-26 12:07:09 +0800

    disable not used brew packages

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "readme.md" --exclude "sublime" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "readme.md" --exclude "sublime" --exclude ".ackrc" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
