079673b2de3b6311b9d75b6d4589ed0183b10af0
Isken Huang <kensheep@gmail.com>
2014-05-26 11:40:30 +0800

    dont copy sublime folder

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "readme.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "readme.md" --exclude "sublime" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
